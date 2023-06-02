f9863e5658d8a48bbe8f64cb2fdb8438fb1db9ea
Benalagat <benardkiplagat2000@mail.com>
2023-06-02 07:52:20 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+
+
+<template>
+  <div class="app">
+    <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+  </div>
+  <RouterView />
+</template>
+
+<style>
+  .app{
+    text-align: center;
+    margin-top: 0%;
+  }
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <button 
+    @click="storeCounter.increment"
+    class="counter-button">
+      {{ storeCounter.count }}
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { useCounterStore } from '@/stores/counter';
+
+const storeCounter=useCounterStore()
+
+</script>
+<style>
+.counter-button{
+  font-size: 50px;
+}
+</style>
+
+
